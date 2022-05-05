6f31b8fb172562e91478c28ed40ac3b9843a5b50
Your Name <you@example.com>
2022-05-05 11:38:25 +0200

    prg jue 05 may 2022 11:38:25 CEST

--- static/JavaScript/cookies.js
+++ static/JavaScript/cookies.js
@@ -1,8 +1,12 @@
 function hazUnaVez() {
     if (document.cookie.replace(/(?:(?:^|.*;\s*)fdafasafasddfasdf\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
-      document.getElementById('modal_cookie_button').click()
-      // alert("evento load detectado!");
-      document.cookie = "fdafasafasddfasdf=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure";
+        document.getElementById('modal_cookie_button').click()
+        // alert("evento load detectado!");
+        document.cookie = "fdafasafasddfasdf=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; SameSite=None; Secure";
     }
-  }
-  window.onload = hazUnaVez;
+}
+
+
+function formularioalasnarices() { document.getElementById('boton_modal_formulario_hogar').click() }
+
+window.onload = hazUnaVez; formularioalasnarices
